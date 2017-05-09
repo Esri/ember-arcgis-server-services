2a78a1c121a1a12026651866200074766d36ce70
dmfenton <dfenton@esri.com>
2017-05-09 11:46:51 -0400

    case insensitive matching

--- addon/utils/should-add-token.js
+++ addon/utils/should-add-token.js
@@ -1,5 +1,5 @@
 export default function shouldAddToken (url, portalId) {
   // if this is a hosted service that belongs to the user's organization
   // if this is a service with stored credentials
-  return !!url.match(`arcgis.com/${portalId}/arcgis/rest`) || /utility\w+\.arcgis\.com\/sharing\/servers\/\w{32}\/rest\/services\/Hosted/.test(url);
+  return !!url.toLowerCase().match(`arcgis.com/${portalId}/arcgis/rest`) || /utility\w+\.arcgis\.com\/sharing\/servers\/\w{32}\/rest\/services\/Hosted/i.test(url);
 }
